24e5264a6951554bdcbf1bb86ce304eeff0c9c1b
PraveenV012 <praveenpraveen1350@gmail.com>
2022-11-02 18:03:40 +0530

    Master codes

---
+++ src/org/practice/PracticeClass.java
@@ -0,0 +1,15 @@
+package org.practice;
+
+public class PracticeClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Practice Class by Master");
+		
+		
+		
+		
+	}
+	
+	
+}
